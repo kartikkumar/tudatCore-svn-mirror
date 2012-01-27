1828de366c7a59ca599db1c2c69352d87eea8fb0
Kartik Kumar <me@kartikkumar.com>
2012-01-27 18:24:23 +0000

    Commit of unit tests transferred to Boost unit test framework.
    git-svn-id: file:///srv/svn/repos/tudatCore/trunk@7 f1678324-46bc-11e1-bac6-37db1e3cb3f9

--- Astrodynamics/unitTestAstrodynamicsFunctions.cpp
+++ Astrodynamics/unitTestAstrodynamicsFunctions.cpp
@@ -2,7 +2,7 @@
  *    This unit test will test general astrodynamics functions defined in astrodynamicsFunctions.h.
  *
  *    Path              : /Astrodynamics/
- *    Version           : 4
+ *    Version           : 5
  *    Check status      : Checked
  *
  *    Author            : K. Kumar
@@ -48,24 +48,26 @@
  *      111122    K. Kumar          Altered tests to use reference data.
  *      111206    K. Kumar          Updated synodic period test and comments.
  *      120127    D. Dirkx          Moved to Tudat core.
+ *      120127    K. Kumar          Transferred unit tests over to Boost unit test framework.
  */
 
+// Required Boost unit test framework define.
+#define BOOST_TEST_MAIN
+
 // Include statements.
+#include <boost/test/unit_test.hpp>
 #include <cmath>
 #include <iostream>
 #include <limits>
 #include "Astrodynamics/astrodynamicsFunctions.h"
 #include "Astrodynamics/physicalConstants.h"
 
-//! Check if the orbital period of a Kepler orbit is computed as expected
-/**
- * \param Return true if the test fail to meet the required test accuracy 
- *               (something does not work as expected)
- */
-bool testKeplerOrbitalPeriod( )
-{
-    bool  isAstrodynamicsFunctionErroneous = false;
+// Define Boost test suite.
+BOOST_AUTO_TEST_SUITE( test_astrodynamics_functions )
 
+//! Test if the orbital period of a Kepler orbit is computed correctly.
+BOOST_AUTO_TEST_CASE( testKeplerOrbitalPeriod )
+{
     // Declare and set satellite mass [kg].
     double satelliteMass = 1.0e3;
 
@@ -84,28 +86,14 @@ bool testKeplerOrbitalPeriod( )
     double expectedOrbitalPeriod = 86164.09054;
 
     // Check if computed orbital period matches expected orbital period.
-    if ( std::fabs( ( orbitalPeriod - expectedOrbitalPeriod ) / expectedOrbitalPeriod ) > 1.0e-5 )
-    {
-        std::cerr << "The two-body orbital period function is not functioning well, as the "
-                  << "computed orbital period (" << orbitalPeriod << ") varies from the expected "
-                  << "orbital period (" << expectedOrbitalPeriod << ") by: "
-                  << orbitalPeriod - expectedOrbitalPeriod << std::endl;
-         isAstrodynamicsFunctionErroneous = true;
-    }
-
-    return  isAstrodynamicsFunctionErroneous;
-
+    BOOST_CHECK( std::fabs( ( orbitalPeriod - expectedOrbitalPeriod )
+                            / expectedOrbitalPeriod ) < 1.0e-5 );
 }
 
-//! Check if the orbital angular momentum of a kepler orbit is computed correctly
-/**
- * \param Return true if the test fail to meet the required test accuracy 
- *               (something does not work as expected)
- */
-bool testKeplerAngularMomentum( )
-{   // Reference: http://en.wikipedia.org/wiki/Geostationary_orbit.
-    bool  isAstrodynamicsFunctionErroneous = false;
-
+//! Test if the orbital angular momentum of a kepler orbit is computed correctly.
+BOOST_AUTO_TEST_CASE( testKeplerAngularMomentum )
+{
+    // Reference: http://en.wikipedia.org/wiki/Geostationary_orbit.
     // Declare and set satellite mass [kg].
     double satelliteMass = 1.0e3;
 
@@ -119,6 +107,7 @@ bool testKeplerAngularMomentum( )
     // Declare and set eccentricity of satellite orbit.
     double eccentricityOfSatelliteOrbit = 0.0;
 
+    // Compute Kepler angular momentum.
     double angularMomentum = tudat::astrodynamics::computeKeplerAngularMomentum(
                 distanceBetweenSatelliteAndEarth, eccentricityOfSatelliteOrbit,
                 earthGravitationalParameter, satelliteMass );
@@ -131,27 +120,14 @@ bool testKeplerAngularMomentum( )
             * std::sqrt( earthGravitationalParameter / distanceBetweenSatelliteAndEarth );
 
     // Check if computed angular momentum matches expected angular momentum.
-    if ( std::fabs( ( angularMomentum - expectedAngularMomentum ) / expectedAngularMomentum )
-         > std::numeric_limits< double >::epsilon( ) )
-    {
-        std::cerr << "The two-body angular momentum function is not functioning well, as the "
-                  << "computed angular momentum (" << angularMomentum << ") varies from the "
-                  << "expected angular momentum (" << expectedAngularMomentum << ") by: "
-                  << angularMomentum - expectedAngularMomentum << std::endl;
-         isAstrodynamicsFunctionErroneous = true;
-    }
-    return  isAstrodynamicsFunctionErroneous;
+    BOOST_CHECK( std::fabs( ( angularMomentum - expectedAngularMomentum )
+                            / expectedAngularMomentum )
+                 < std::numeric_limits< double >::epsilon( ) );
 }
 
-//! Test of the mean motion of a kepler orbit is computed correctly
-/**
- * \param Return true if the test fail to meet the required test accuracy 
- *               (something does not work as expected)
- */
-bool testMeanMotion( )
+//! Test if the mean motion of a kepler orbit is computed correctly.
+BOOST_AUTO_TEST_CASE( testMeanMotion )
 {
-    bool  isAstrodynamicsFunctionErroneous = false;
-
     // Declare and set satellite mass [kg].
     double satelliteMass = 1.0e3;
 
@@ -170,26 +146,12 @@ bool testMeanMotion( )
     double expectedMeanMotion = 7.2921e-5;
 
     // Check if computed mean motion matches expected mean motion.
-    if ( std::fabs( ( meanMotion - expectedMeanMotion ) / expectedMeanMotion ) > 1.0e-7 )
-    {
-        std::cerr << "The two-body mean motion function is not functioning well, as the "
-                  << "computed mean motion (" << meanMotion << ") varies from the "
-                  << "expected mean motion (" << expectedMeanMotion << ") by: "
-                  << meanMotion - expectedMeanMotion << std::endl;
-         isAstrodynamicsFunctionErroneous = true;
-    }
-    return  isAstrodynamicsFunctionErroneous;
+    BOOST_CHECK( std::fabs( ( meanMotion - expectedMeanMotion ) / expectedMeanMotion ) < 1.0e-7 );
 }
 
-//! Check of the Orbital energy of a Kepler orbit is computed correctly
-/**
- * \param Return true if the test fail to meet the required test accuracy 
- *               (something does not work as expected)
- */
-bool testKeplerEnergy( )
+//! Test if the orbital energy of a Kepler orbit is computed correctly.
+BOOST_AUTO_TEST_CASE( testKeplerEnergy )
 {
-    bool  isAstrodynamicsFunctionErroneous = false;
-
     // Declare and set satellite mass [kg].
     double satelliteMass = 1.0e3;
 
@@ -200,6 +162,7 @@ bool testKeplerEnergy( )
     // Declare and set distance between Earth center and satellite.
     double distanceBetweenSatelliteAndEarth = 4.2164e7;
 
+    // Compute Kepler energy.
     double orbitalEnergy = tudat::astrodynamics::computeKeplerEnergy(
                 distanceBetweenSatelliteAndEarth, earthGravitationalParameter, satelliteMass );
 
@@ -212,86 +175,26 @@ bool testKeplerEnergy( )
                 -  earthGravitationalParameter / distanceBetweenSatelliteAndEarth );
 
     // Check if computed orbital energy matches expected orbital energy.
-    if ( std::fabs( ( orbitalEnergy - expectedOrbitalEnergy ) / expectedOrbitalEnergy )
-         > std::numeric_limits< double >::epsilon( ) )
-    {
-        std::cerr << "The two-body orbital energy function is not functioning well, as the "
-                  << "computed orbital energy (" << orbitalEnergy << ") varies from the "
-                  << "expected orbital energy (" << expectedOrbitalEnergy << ") by: "
-                  << orbitalEnergy - expectedOrbitalEnergy << std::endl;
-         isAstrodynamicsFunctionErroneous = true;
-    }
-    return  isAstrodynamicsFunctionErroneous;
+    BOOST_CHECK( std::fabs( ( orbitalEnergy - expectedOrbitalEnergy ) / expectedOrbitalEnergy )
+                 < std::numeric_limits< double >::epsilon( ) );
 
 }
 
-//! Check if the synodic period between two orbits is commuted correctly
-/**
- * \param Return true if the test fail to meet the required test accuracy 
- *               (something does not work as expected)
- */
-bool testSynodicPeriod( )
+//! Test if the synodic period between two orbits is computed correctly.
+BOOST_AUTO_TEST_CASE( testSynodicPeriod )
 {
-    bool  isAstrodynamicsFunctionErroneous = false;
-
+    // Compute synodic period between Earth and Mars.
     double synodicPeriod = tudat::astrodynamics::computeSynodicPeriod( 365.256378, 686.95 );
+
     // Declare and set expected synodic period.
     double expectedSynodicPeriod = 779.9746457736733;
 
     // Check if computed synodic period matches expected synodic period.
-    if ( std::fabs( ( synodicPeriod - expectedSynodicPeriod ) / expectedSynodicPeriod )
-         > std::numeric_limits< double >::epsilon( ) )
-    {
-        std::cerr << "The synodic period function is not functioning well, as the "
-                  << "computed synodic period (" << synodicPeriod << ") varies from the "
-                  << "expected synodic period (" << expectedSynodicPeriod << ") by: "
-                  << synodicPeriod - expectedSynodicPeriod << std::endl;
-         isAstrodynamicsFunctionErroneous = true;
-    }
-    return  isAstrodynamicsFunctionErroneous;
+    BOOST_CHECK( std::fabs( ( synodicPeriod - expectedSynodicPeriod ) / expectedSynodicPeriod )
+                 < std::numeric_limits< double >::epsilon( ) );
 }
 
-//! Test implementation of general astrodynamics functions.
-int main( )
-{
-    // Declare and initialize test result to false.
-    bool  isAstrodynamicsFunctionsErroneous = false;
+// Close Boost test suite.
+BOOST_AUTO_TEST_SUITE_END( )
 
-    // Test 1: Compute orbital period for geostationary satellite test case.
-    if ( testKeplerOrbitalPeriod( ) )
-    {
-        isAstrodynamicsFunctionsErroneous = true;
-    }
-
-    // Test 2: Compute two-body angular momentum for geostationary satellite test case.
-    if ( testKeplerAngularMomentum( ) )
-    {
-        isAstrodynamicsFunctionsErroneous = true;
-    }
-
-    // Test 3: Compute two-body mean motion for geostationary satellite test case.
-    if ( testMeanMotion( ) )
-    {
-        isAstrodynamicsFunctionsErroneous = true;
-    }
-
-    // Test 4: Compute two-body orbital energy for geostationary satellite test case.
-    if ( testKeplerEnergy( ) )
-    {
-        isAstrodynamicsFunctionsErroneous = true;
-    }
-
-    // Test 5: Compute synodic period of two bodies in different orbits.
-    if ( testSynodicPeriod( ) )
-    {
-        isAstrodynamicsFunctionsErroneous = true;
-    }
-
-    // Return a message if a test has failed.
-    if ( isAstrodynamicsFunctionsErroneous )
-    {
-        std::cerr << "testAstrodynamicsFunctions failed!" << std::endl;
-    }
-    // Return a non-zero exit code if any test failed
-    return isAstrodynamicsFunctionsErroneous;
-}
+// End of file.
