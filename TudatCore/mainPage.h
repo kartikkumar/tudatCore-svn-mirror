f34a05da98710fbe57aba326b06925637e119726
Kartik Kumar <me@kartikkumar.com>
2012-02-18 14:37:41 +0000

    Commit of mainPage.h for Doxygen mainPage. Includes svn:keywords.
    git-svn-id: file:///srv/svn/repos/tudatCore/trunk@92 f1678324-46bc-11e1-bac6-37db1e3cb3f9

---
+++ TudatCore/mainPage.h
@@ -0,0 +1,18 @@
+/*! \mainpage
+ * Tudat Core is an astrodynamics library, providing various features, including numerical
+ * integrators, orbital element conversions, unit conversions, and various other standard
+ * functionality used by astrodynamicists.
+ *
+ * The Tudat Core library is part of the Tudat project, and is maintained by staff and students in
+ * the Astrodynamics & Space missions research group, at the Faculty of Aerospace Engineering,
+ * Delft University of Technology, The Netherlands. For more information vist:
+ * http://tudat.tudelft.nl/.
+ *
+ * This Doxygen website provides an overview of the various code interfaces incorporated in Tudat
+ * Core. The Doxygen website is updated regularly.
+ *
+ * $Rev::               $:  Revision of last commit.
+ * $Date::              $:  Date of last commit.
+ *
+ * Please address any queries regarding Tudat Core to tudat-AE@tudelft.nl.
+ */
