bbe6b74d13406fa87c50f83906e8b9c24d2eaa71
Kartik Kumar <me@kartikkumar.com>
2013-02-05 02:58:22 +0000

    Updated FindBoost.cmake and FindEigen3.cmake CMake modules to detect Boost and Eigen out of the box for Tudat and any applications.
    git-svn-id: file:///srv/svn/repos/tudatCore/trunk@159 f1678324-46bc-11e1-bac6-37db1e3cb3f9

--- TudatCore/External/CMake/FindBoost.cmake
+++ TudatCore/External/CMake/FindBoost.cmake
@@ -606,6 +606,7 @@ set(Boost_ERROR_REASON)
     "$ENV{ProgramFiles}/boost"
     /sw/local/include
     "${PROJECT_SOURCE_DIR}/../boost"
+    "${PROJECT_SOURCE_DIR}/../../tudatCore/boost"
   )
 
   _Boost_CHECK_SPELLING(Boost_ROOT)
@@ -867,6 +868,7 @@ set(Boost_ERROR_REASON)
     "$ENV{ProgramFiles}/boost"
     /sw/local/lib
     "${PROJECT_SOURCE_DIR}/../boost/stage/lib"
+    "${PROJECT_SOURCE_DIR}/../../tudatCore/boost"
   )
   set(_boost_LIBRARY_SEARCH_DIRS ${_boost_LIBRARY_SEARCH_DIRS_ALWAYS})
   if( Boost_NO_SYSTEM_PATHS )
